09fde92cc1c8aa6ac39c05a54013b0c876e37971
Anika Gunaselan <gunaselananika@gmail.com>
2024-06-11 13:08:52 -0500

    Initial Commit

---
+++ OnTimeHomePage/OnTimeHomePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTimeHomePageApp.swift
+//  OnTimeHomePage
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OnTimeHomePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
